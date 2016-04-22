1f5891e358886c7f78de64a85ff6a27e0b0c6c8c
jaypatel28 <patel.jay@aol.com>
2016-04-22 12:39:51 -0400

    New UITableViewController with expandable cell with Buttons

---
+++ ExpandableCell/PickerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PickerTableViewCell.swift
+//  ExpandableCell
+//
+//  Created by Jay Patel on 4/22/16.
+//  Copyright © 2016 MEAMobile. All rights reserved.
+//
+
+import UIKit
+
+class PickerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
